8367bd65a35e744915a667a47d2d200c72eb4d2b
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-16 10:32:49 +0000

    card flash with sound added

--- script.js
+++ script.js
@@ -50,4 +50,72 @@ function play() {
         gameSequence.push(Math.floor(Math.random() * 4) + 1);
     }
     console.log(gameSequence);
+    gameTurn = true;
+    intervalId = setInterval(gameFlash, 800);
+}
+
+function gameFlash() {
+    gameOn = false;
+
+    if (backlight == turn) {
+        clearInterval(intervalId);
+        gameTurn = false;
+        clearColor;
+        gameOn = true;
+    }
+
+    if (gameTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (gameSequence[backlight] == 1) one();
+            if (gameSequence[backlight] == 2) two();
+            if (gameSequence[backlight] == 3) three();
+            if (gameSequence[backlight] == 4) four();
+            backlight++;
+        }, 200);
+    }
+}
+
+function one() {
+    if (sound) {
+        let audio = document.getElementById("sound-one")
+        audio.play();
+    }
+    sound = true;
+    flashGreen.style.backgroundColor = 'lightgreen';
+}
+
+function two() {
+    if (sound) {
+        let audio = document.getElementById("sound-two")
+        audio.play();
+    }
+    sound = true;
+    flashRed.style.backgroundColor = 'tomato';
+}
+
+function three() {
+    if (sound) {
+        let audio = document.getElementById("sound-three")
+        audio.play();
+    }
+    sound = true;
+    flashYellow.style.backgroundColor = 'orange';
+}
+
+
+function four() {
+    if (sound) {
+        let audio = document.getElementById("sound-four")
+        audio.play();
+    }
+    sound = true;
+    flashBlue.style.backgroundColor = 'lightskyblue';
+}
+
+function clearColor() {
+    flashGreen.style.backgroundColor = 'darkcolor';
+    flashRed.style.backgroundColor = 'darkred';
+    flashYellow.style.backgroundColor = 'goldenrod';
+    flashBlue.style.backgroundColor = 'darkblue';
 }
